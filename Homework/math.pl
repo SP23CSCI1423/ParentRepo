62fb7d1bd0ede004de11009a02529e6c81739c09
PSanthosh45 <122559629+PSanthosh45@users.noreply.github.com>
2023-01-12 12:47:24 -0600

    Delete math.pl

--- Homework/math.pl
+++
@@ -1,7 +0,0 @@
-#Prompt users for two numbers and print five sentences that show those numbers being:
-# 1. Added
-# 2. Subtracted
-# 3. Multiplied
-# 4. Divided
-# 5. Exponents
-#Example Output (for adding if user gave numbers 3 and 4): 3 plus 4 equals 7
