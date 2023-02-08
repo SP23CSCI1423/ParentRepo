f611262f50b7fb4602fd3455117bec96bca44b80
KEdwards25 <122554995+KEdwards25@users.noreply.github.com>
2023-02-08 17:54:36 +0000

    2/8 Lecture

---
+++ Labs/Lab2.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+#Make two arrays
+#First array should have three family members names and ages (so you could have their first name, then their age)
+#Print the entire array, then everyone's age
+#Second array should have three movies and their release date (so their title, then the release date)
+#Print the entire array, then print the second movie and it's release date
+
+#NOTE: Names and ages, and then movies and release date, should be separate variables (so like ("john", 34) for example)
+#NOTE: The print statements should be separate
+
