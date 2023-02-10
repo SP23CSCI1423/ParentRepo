ed35497f394dce9d2173e2f17d9ce4299c1850a7
KEdwards25 <122554995+KEdwards25@users.noreply.github.com>
2023-02-10 05:45:58 +0000

    Lab1

---
+++ Labs/Lab1.pl
@@ -0,0 +1,12 @@
+#Lab 1 2/6/2023
+
+###Tasks
+#For each of the given tasks, save as a separate variable
+#Ask for first name
+#Ask for last name
+#Ask for age
+#Ask for date of birth
+#Ask for hair color
+
+#After asking for each of these things, print the input
+###NOTE: Printing should clearly relate to the variable (ex: Your first name is [input])
