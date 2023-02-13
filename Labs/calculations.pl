87c54781a5806a1b2483e3f38455ccbf0e2a8508
KEdwards25 <122554995+KEdwards25@users.noreply.github.com>
2023-02-13 17:50:32 +0000

    Calculations Lab

---
+++ Labs/calculations.pl
@@ -0,0 +1,6 @@
+#Ask the user for two numbers
+#Run the numbers through the calculations we've gone over (add, subtract, multiply, divide, exponents, mod)
+#Print the results of each
+#Make an array called "results"
+#Put the results from the calculations into the array
+#Print the results array
