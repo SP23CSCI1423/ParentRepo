0be5162cfb96e7fd5140f247b659411c32868d18
KEdwards25 <122554995+KEdwards25@users.noreply.github.com>
2023-02-15 17:49:41 +0000

    Lab0215

---
+++ Labs/RocketLaunch.pl
@@ -0,0 +1,11 @@
+#Call it RocketLaunch.pl
+
+#Using a loop, countdown from 10 to 0
+#Print each number
+#At the end, print "Blast off"
+
+#When you run it, it should look like this
+#  10
+#  9
+#  8
+# so on until you reach zero, then print "Blast off"
